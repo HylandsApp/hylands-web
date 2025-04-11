0cf6f873872d7c984b7b8ba9fc8fcb94a5f5275b
Eric Rose <admin@hylands.app>
2025-04-11 14:02:23 -0700

    removes example code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    about
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style lang="scss" scoped>
+  // style here
 </style>
+
+<script setup lang="ts">
+ // script here
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    home
   </main>
 </template>
+
+<style lang="scss" scoped>
+  // style here
+</style>
+
+<script setup lang="ts">
+ // script here
+</script>
